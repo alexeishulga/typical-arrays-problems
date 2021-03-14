648b300e8f2904d239915d7081a6bebe13f38a8a
alexeishulga <alexeishulg@gmail.com>
2021-03-14 15:24:09 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+  if (!array || !array.length) {
+      return 0;
+  } else {
+      let minVal = array[0];
+      for (let i = 0; i < array.length; i++) {
+          if (minVal > array[i]) {
+              minVal = array[i];
+          }
+      }
+      return minVal;
+  }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || !array.length) {
+      return 0;
+  } else {
+      let maxVal = array[0];
+      for (let i = 0; i < array.length; i++) {
+          if (maxVal < array[i]) {
+              maxVal = array[i];
+          }
+      }
+      return maxVal;
+  }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  let sum = 0;
+  if (!array || !array.length) {
+      return 0;
+  } else {
+      for (let i = 0; i < array.length; i++) {
+          sum += array[i];
+      }
+      let average = sum / array.length;
+      return average;
+  }
+};
